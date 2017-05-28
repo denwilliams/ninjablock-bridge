eb1accce79905184612360ee33c28387bf5b3afa
Den Williams <work@denwilliams.net>
2017-05-28 21:25:35 +1000

    log message

--- bin/ninjabridge.js
+++ bin/ninjabridge.js
@@ -4,4 +4,5 @@ bridge.onAny(function(evt) {
   var message = evt.event;
   if (evt.deviceName) message += (' (' + evt.deviceName + ')');
   message += ' > ' + evt.data;
+  console.log(message);
 });
